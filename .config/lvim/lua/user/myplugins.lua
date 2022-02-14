8fb4258b3a77fe51b994530111959afc46514349
Jaime Aleman <organizedfellow@gmail.com>
2022-02-14 00:20:01 -0700

    lvim

---
+++ .config/lvim/lua/user/myplugins.lua
@@ -0,0 +1,25 @@
+-- Additional Plugins
+lvim.plugins = {
+--     {"folke/tokyonight.nvim"},
+--     {
+--       "folke/trouble.nvim",
+--       cmd = "TroubleToggle",
+--     },
+--  {"wakatime/vim-wakatime"},       -- :WakaTimeToday
+  {"Pocco81/AutoSave.nvim"},        -- :
+  {"Mofiqul/dracula.nvim"},
+  {"lukas-reineke/indent-blankline.nvim"},
+  {"mattn/emmet-vim"},
+  {"ActivityWatch/aw-watcher-vim"},
+--:AWStart - start logging if not already logging
+--:AWStop - stop logging if logging
+--:AWStatus - verify that the watcher is running
+  {"hrsh7th/cmp-path"},
+  {"ThePrimeagen/vim-be-good"},
+  {"ap/vim-css-color"},
+  {"Yilin-Yang/vim-markbar"},
+  {"rafamadriz/friendly-snippets"},
+  {"https://gitlab.com/code-stats/code-stats-vim.git"},
+  {"unblevable/quick-scope"}
+}
+
